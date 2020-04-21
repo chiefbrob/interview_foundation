4b339d61c0d64dc3d27e5a5270638f525c357fe0
Obare C. Brian <brianobare@gmail.com>
2020-04-21 14:14:47 +0300

    typo

--- app/Http/Controllers/GitHub/DeleteToken.php
+++ app/Http/Controllers/GitHub/DeleteToken.php
@@ -16,7 +16,7 @@ class DeleteToken extends Controller
     public function __invoke(Request $request)
     {
         $user = Auth::user();
-        $user->deleteGitHubToken()
+        $user->deleteGitHubToken();
         return response([ 'message' => 'ok' ], 202);
     }
 }
