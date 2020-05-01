59a50d7fbf6d775d1053ed42a097338ab4ad6c30
Brian Chief Obare <brianobare@gmail.com>
2020-05-01 18:04:49 +0300

    mocking github

--- tests/Feature/GitHubAPI/GetStarredRepositoriesTest.php
+++ tests/Feature/GitHubAPI/GetStarredRepositoriesTest.php
@@ -7,22 +7,30 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Session;
 
+use Tests\Feature\MocksGitHubAPI;
+use App\User;
+
 class GetStarredRepositoriesTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, MocksGitHubAPI;
 
     public function testGetStarredRepos()
     {
-        Session::start();
-        $user = factory('App\User')->create([
-            'github_token' => encrypt(env('GITHUB_TOKEN')),
-        ]);
+        $this->addMockHttpResponse(json_encode([]));
+        $this->actingAs(factory(User::class)->create(['github_token' => 'some-token']))
+            ->getJson('/repositories/starred')
+            ->assertStatus(200)
+            ->assertOk();
+        // Session::start();
+        // $user = factory('App\User')->create([
+        //     'github_token' => encrypt(env('GITHUB_TOKEN')),
+        // ]);
 
-        $response = $this->actingAs($user)->get('/repositories/starred', [
-            '_token' => csrf_token()
-        ]);
+        // $response = $this->actingAs($user)->get('/repositories/starred', [
+        //     '_token' => csrf_token()
+        // ]);
 
-        $response->assertStatus(200);
-        $response->assertSeeText('ok');
+        // $response->assertStatus(200);
+        // $response->assertSeeText('ok');
     }
 }
